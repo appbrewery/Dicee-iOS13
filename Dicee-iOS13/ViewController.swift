249e734aea0d3f3f64f9e4ecba66f1c45feca047
AzinT <ataqizad@uwaterloo.ca>
2020-04-10 15:59:00 -0400

    up to lesson 42

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // drag and drop the ui elements here:
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // what happenes when app loads
+        
+        // format: who.what = value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // used image literal here
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("button got tapped")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
