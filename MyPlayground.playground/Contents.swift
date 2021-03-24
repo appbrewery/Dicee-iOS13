5ba1a12ef1e82712eca07c64002632b3b5bc2092
EleanorKal <eleanor@blarnya.com>
2021-03-24 13:32:27 +0100

    Dice Game
    Updates to Dicee-iOS13

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,47 @@
+
+// This is a comment
+
+//print("Hello World!")
+
+/* this is a single line
+ this is a multiline comment
+ */
+
+// When adding code to a string is known as String Interpolation
+//print("Hello \(2+3) World")
+//print("The result of 5 + 3 = \(5+3)")
+
+//var a = 5
+//var b = 8
+//var c = b
+//print("The value of a is \(a)")
+//print("The value of a is \(b)")
+//
+//
+//b = a
+//a = c
+//print("The new value of A is: \(a)")
+//print("The new value of B is: \(b)")
+
+//var friends = ["Angela", "Jack", "Philipp", "Stephanie", "Mike"][1]
+//print(friends)
+
+//Play calc area:
+// var computedNumbers = (numbers[0]) * (numbers[1])
+//var computedNumbersTwo = computedNumbers * numbers[2]
+//var computedNumbersThree = computedNumbersTwo * numbers[3]
+let numbers = [45, 73, 195, 53]
+let computedNumbers = ((numbers[0]) * (numbers[1]) ,
+                 (numbers[1]) * (numbers[2]),
+                 (numbers[2]) * (numbers[3]))
+
+print(computedNumbers)
+
+//Sanity check the computedNunmbers is correct
+//print(45*73, 73*195, 195*53)
+
+
+
+
+
+
