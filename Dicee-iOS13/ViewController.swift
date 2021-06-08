44ee0dd3be67f9d737c2dc4290b28adc020bdb43
Andrew <andrewpgannon1@gmail.com>
2021-06-07 22:19:38 -0400

    h

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
