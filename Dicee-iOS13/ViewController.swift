04dbb88063753fb814ccb666a948616b138c2d4b
Mariana Piz <marianapiz0412@gmail.com>
2023-07-09 17:40:25 +0300

    Create IBOutlets for dice images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
